06e25d51b11b78baf52d6d0fd60f702df7b3cad4
Jo Kerkhofs <jke@thinkerit.be>
2016-06-07 14:59:08 +0200

    Manage angular dependency through angularx module. Define sources.

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
         files:[
             {pattern:'bower_components/angular/angular.js'},
             {pattern:'bower_components/angular-mocks/angular-mocks.js'},
-            {pattern:'src/**/*.js'},
+            {pattern:'src/edit.js'},
             {pattern:'test/**/*.*'}
         ],
         browsers:['PhantomJS']
