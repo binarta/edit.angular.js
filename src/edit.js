07a03b820ed041e18afdba0231859e93be98934a
Jo Kerkhofs <jke@thinkerit.be>
2017-06-27 16:06:57 +0200

    refactor

--- src/edit.js
+++ src/edit.js
@@ -15,7 +15,90 @@
             'header': '?binEditHeader',
             'body': 'binEditBody'
         };
-        this.controller = ['topicRegistry', BinEditController];
+        this.controller = ['topicRegistry', function (topics) {
+            var $ctrl = this;
+            var actionsListeners = [];
+            var states = {
+                hidden: function () {
+                    this.name = 'hidden';
+                    this.close = function () {}
+                },
+                closed: function (fsm) {
+                    this.name = 'closed';
+                    $ctrl.showActionsFor();
+                    this.toggle = function () {
+                        fsm.state = new states.opened(fsm);
+                    };
+                    this.close = function () {}
+                },
+                opened: function (fsm) {
+                    this.name = 'opened';
+                    this.toggle = function () {
+                        fsm.state = new states.closed(fsm);
+                    };
+                    this.close = function () {
+                        fsm.state = new states.closed(fsm);
+                    };
+                }
+            };
+
+            $ctrl.$onInit = function () {
+                $ctrl.state = new states.hidden();
+
+                $ctrl.close = function () {
+                    $ctrl.state.close();
+                };
+
+                $ctrl.toggle = function () {
+                    $ctrl.state.toggle();
+                };
+
+                $ctrl.startWorking = function () {
+                    $ctrl.working = true;
+                };
+
+                $ctrl.stopWorking = function () {
+                    $ctrl.working = false;
+                };
+
+                $ctrl.showActionsFor = function (id) {
+                    actionsListeners.forEach(function (cb) {
+                        cb(id);
+                    });
+                };
+
+                $ctrl.onShowActionsFor = function (cb) {
+                    actionsListeners.push(cb);
+                };
+
+                $ctrl.setButtonCode = function (code) {
+                    $ctrl.buttonCode = code;
+                };
+
+                var editModeListener = function (editModeActive) {
+                    $ctrl.editing = editModeActive;
+                    initState();
+                };
+
+                topics.subscribe('edit.mode', editModeListener);
+
+                $ctrl.$onDestroy = function () {
+                    topics.unsubscribe('edit.mode', editModeListener);
+                };
+            };
+
+            $ctrl.$onChanges = function () {
+                initState();
+            };
+
+            function isEditable() {
+                return $ctrl.editing && ($ctrl.isEditable === undefined || $ctrl.isEditable);
+            }
+
+            function initState() {
+                $ctrl.state = isEditable() ? new states.closed($ctrl) : new states.hidden();
+            }
+        }];
     }
 
     function BinEditActionsComponent() {
@@ -103,90 +186,5 @@
                 $ctrl.working = false;
             }
         };
-    }
-
-    function BinEditController(topics) {
-        var $ctrl = this;
-        var actionsListeners = [];
-        var states = {
-            hidden: function () {
-                this.name = 'hidden';
-                this.close = function () {}
-            },
-            closed: function (fsm) {
-                this.name = 'closed';
-                $ctrl.showActionsFor();
-                this.toggle = function () {
-                    fsm.state = new states.opened(fsm);
-                };
-                this.close = function () {}
-            },
-            opened: function (fsm) {
-                this.name = 'opened';
-                this.toggle = function () {
-                    fsm.state = new states.closed(fsm);
-                };
-                this.close = function () {
-                    fsm.state = new states.closed(fsm);
-                };
-            }
-        };
-
-        this.$onInit = function () {
-            $ctrl.state = new states.hidden();
-
-            $ctrl.close = function () {
-                $ctrl.state.close();
-            };
-
-            $ctrl.toggle = function () {
-                $ctrl.state.toggle();
-            };
-
-            $ctrl.startWorking = function () {
-                $ctrl.working = true;
-            };
-
-            $ctrl.stopWorking = function () {
-                $ctrl.working = false;
-            };
-
-            $ctrl.showActionsFor = function (id) {
-                actionsListeners.forEach(function (cb) {
-                    cb(id);
-                });
-            };
-
-            $ctrl.onShowActionsFor = function (cb) {
-                actionsListeners.push(cb);
-            };
-
-            $ctrl.setButtonCode = function (code) {
-                $ctrl.buttonCode = code;
-            };
-
-            var editModeListener = function (editModeActive) {
-                $ctrl.editing = editModeActive;
-                initState();
-            };
-
-            topics.subscribe('edit.mode', editModeListener);
-
-            $ctrl.$onDestroy = function () {
-                topics.unsubscribe('edit.mode', editModeListener);
-            };
-        };
-
-        $ctrl.$onChanges = function () {
-            initState();
-        };
-
-        function isEditable() {
-            return $ctrl.editing && ($ctrl.isEditable === undefined || $ctrl.isEditable);
-        }
-
-        function initState() {
-            $ctrl.state = isEditable() ? new states.closed($ctrl) : new states.hidden();
-        }
     }
 })();
