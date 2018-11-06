caeb48e28d417d6ce504d9249026dd655ce681e9
wro <wouter@websters.be>
2018-11-06 13:05:42 +0100

    bin-edit now uses bin-actions instead of defining it's own sub components

--- src/edit.js
+++ src/edit.js
@@ -1,9 +1,6 @@
 (function () {
-    angular.module('bin.edit', ['notifications'])
-        .component('binEdit', new BinEditComponent())
-        .component('binEditActions', new BinEditActionsComponent())
-        .component('binEditActionsSelector', new BinEditActionsSelectorComponent())
-        .component('binEditAction', new BinEditActionComponent());
+    angular.module('bin.edit', ['notifications', 'angularx'])
+        .component('binEdit', new BinEditComponent());
 
     function BinEditComponent() {
         this.templateUrl = 'bin-edit.html';
@@ -11,93 +8,45 @@
             isEditable: '<?'
         };
         this.transclude = {
-            'actions': 'binEditActions',
+            'actions': 'binActions',
             'header': '?binEditHeader',
             'body': 'binEditBody'
         };
-        this.controller = ['topicRegistry', function (topics) {
+        this.controller = ['$element', '$scope', 'topicRegistry', function ($element, $scope, topics) {
             var $ctrl = this;
-            var actionsListeners = [];
-            var mainActions = 0;
             var states = {
+                visible: function() {
+                    this.name = 'visible';
+                },
                 hidden: function () {
                     this.name = 'hidden';
-                    this.close = function () {}
-                },
-                closed: function (fsm) {
-                    this.name = 'closed';
-                    $ctrl.showActionsFor();
-                    this.toggle = function () {
-                        fsm.state = new states.opened(fsm);
-                    };
-                    this.close = function () {}
-                },
-                opened: function (fsm) {
-                    this.name = 'opened';
-                    this.toggle = function () {
-                        fsm.state = new states.closed(fsm);
-                    };
-                    this.close = function () {
-                        fsm.state = new states.closed(fsm);
-                    };
                 }
+            };
+            var unsubscribes = [];
+
+            var editModeListener = function (editModeActive) {
+                $ctrl.editing = editModeActive;
+                initState();
             };
 
             $ctrl.$onInit = function () {
                 $ctrl.state = new states.hidden();
-
-                $ctrl.close = function () {
-                    $ctrl.state.close();
-                };
-
-                $ctrl.toggle = function () {
-                    $ctrl.state.toggle();
-                };
-
-                $ctrl.startWorking = function () {
-                    $ctrl.working = true;
-                };
-
-                $ctrl.stopWorking = function () {
-                    $ctrl.working = false;
-                };
-
-                $ctrl.showActionsFor = function (id) {
-                    actionsListeners.forEach(function (cb) {
-                        cb(id);
-                    });
-                };
-
-                $ctrl.onShowActionsFor = function (cb) {
-                    actionsListeners.push(cb);
-                };
-
-                $ctrl.setButtonCode = function (code) {
-                    $ctrl.buttonCode = code;
-                };
-
-                $ctrl.increaseMainActionCount = function () {
-                    mainActions += 1;
-                };
-
-                $ctrl.decreaseMainActionCount = function () {
-                    mainActions -= 1;
-                };
-
-                $ctrl.hasMainActions = function () {
-                    return mainActions > 0;
-                };
-
-                var editModeListener = function (editModeActive) {
-                    $ctrl.editing = editModeActive;
-                    initState();
-                };
-
                 topics.subscribe('edit.mode', editModeListener);
+                unsubscribes.push($scope.$on('bin.actions.opened', function(event) {
+                    event.stopPropagation();
+                    $element.addClass('open');
+                }));
+                unsubscribes.push($scope.$on('bin.actions.closed', function(event) {
+                    event.stopPropagation();
+                    $element.removeClass('open');
+                }));
+            };
 
-                $ctrl.$onDestroy = function () {
-                    topics.unsubscribe('edit.mode', editModeListener);
-                };
+            $ctrl.$onDestroy = function () {
+                topics.unsubscribe('edit.mode', editModeListener);
+                unsubscribes.forEach(function(unsubscribe) {
+                    unsubscribe();
+                })
             };
 
             $ctrl.$onChanges = function () {
@@ -109,120 +58,8 @@
             }
 
             function initState() {
-                $ctrl.state = isEditable() ? new states.closed($ctrl) : new states.hidden();
+                $ctrl.state = isEditable() ? new states.visible() : new states.hidden();
             }
         }];
-    }
-
-    function BinEditActionsComponent() {
-        this.template = '<div ng-show="$ctrl.visible" ng-transclude></div>';
-        this.bindings = {
-            for: '@',
-            buttonI18nCode: '@'
-        };
-        this.require = {
-            editCtrl: '^^binEdit'
-        };
-        this.transclude = true;
-
-        this.controller = function () {
-            var $ctrl = this;
-            var actions = [];
-
-            $ctrl.$onInit = function () {
-                if (!$ctrl.for) $ctrl.visible = true;
-                $ctrl.editCtrl.onShowActionsFor(function (id) {
-                    $ctrl.visible = $ctrl.for === id;
-                    if ($ctrl.visible) $ctrl.editCtrl.setButtonCode($ctrl.buttonI18nCode);
-                });
-
-                $ctrl.increaseActionCount = function () {
-                    if (!$ctrl.for) $ctrl.editCtrl.increaseMainActionCount();
-                };
-
-                $ctrl.decreaseActionCount = function () {
-                    if (!$ctrl.for) $ctrl.editCtrl.decreaseMainActionCount();
-                };
-            };
-        };
-    }
-
-    function BinEditActionsSelectorComponent() {
-        this.templateUrl = 'bin-edit-action.html';
-        this.bindings = {
-            for: '@',
-            danger: '@',
-            iconClass: '@',
-            i18nCode: '@'
-        };
-        this.require = {
-            editCtrl: '^^binEdit',
-            actionsCtrl: '^^binEditActions'
-        };
-        this.transclude = true;
-
-        this.controller = function () {
-            var $ctrl = this;
-            
-            $ctrl.$onInit = function () {
-                $ctrl.actionsCtrl.increaseActionCount();
-                
-                $ctrl.execute = function () {
-                    $ctrl.editCtrl.showActionsFor($ctrl.for);
-                };
-
-                $ctrl.$destroy = function () {
-                    $ctrl.actionsCtrl.decreaseActionCount();
-                };
-            };
-        };
-    }
-
-    function BinEditActionComponent() {
-        this.templateUrl = 'bin-edit-action.html';
-        this.bindings = {
-            action: '&',
-            danger: '@',
-            disabled: '<',
-            iconClass: '@',
-            i18nCode: '@'
-        };
-        this.require = {
-            editCtrl: '^^binEdit',
-            actionsCtrl: '^^binEditActions'
-        };
-        this.transclude = true;
-
-        this.controller = function () {
-            var $ctrl = this;
-
-            $ctrl.$onInit = function () {
-                $ctrl.actionsCtrl.increaseActionCount();
-
-                $ctrl.execute = function () {
-                    if (!$ctrl.working && !$ctrl.disabled) {
-                        var result = $ctrl.action();
-                        if (result && result.finally) {
-                            startWorking();
-                            result.finally(stopWorking);
-                        }
-                    }
-                };
-
-                $ctrl.$destroy = function () {
-                    $ctrl.actionsCtrl.decreaseActionCount();
-                };
-            };
-
-            function startWorking() {
-                $ctrl.editCtrl.startWorking();
-                $ctrl.working = true;
-            }
-
-            function stopWorking() {
-                $ctrl.editCtrl.stopWorking();
-                $ctrl.working = false;
-            }
-        };
     }
 })();
--- test/edit.spec.js
+++ test/edit.spec.js
@@ -4,9 +4,12 @@ describe('bin.edit module', function () {
     describe('binEdit component', function () {
         var $ctrl, topics;
 
-        beforeEach(inject(function ($componentController, topicRegistry) {
+        beforeEach(inject(function ($componentController, $rootScope, topicRegistry) {
             topics = topicRegistry;
-            $ctrl = $componentController('binEdit');
+            this.$element = angular.element('<div></div>');
+            this.$scope = $rootScope.$new();
+            this.$child = this.$scope.$new();
+            $ctrl = $componentController('binEdit', {$element: this.$element, $scope: this.$scope});
             $ctrl.$onInit();
         }));
 
@@ -18,96 +21,13 @@ describe('bin.edit module', function () {
             expect(topics.subscribe).toHaveBeenCalledWith('edit.mode', jasmine.any(Function));
         });
 
-        describe('when not in edit mode', function () {
-            beforeEach(function () {
-                topics.subscribe.calls.mostRecent().args[1](false);
-            });
-
-            it('component is in hidden state', function () {
-                expect($ctrl.state.name).toEqual('hidden');
-            });
-
-            it('on close', function () {
-                $ctrl.close();
-
-                expect($ctrl.state.name).toEqual('hidden');
-            });
-        });
-
         describe('when in edit mode', function () {
             beforeEach(function () {
                 topics.subscribe.calls.mostRecent().args[1](true);
             });
 
-            it('component is in closed state', function () {
-                expect($ctrl.state.name).toEqual('closed');
-            });
-
-            it('on toggle menu', function () {
-                $ctrl.state.toggle();
-
-                expect($ctrl.state.name).toEqual('opened');
-
-                $ctrl.toggle();
-
-                expect($ctrl.state.name).toEqual('closed');
-            });
-
-            it('on close', function () {
-                $ctrl.toggle();
-
-                expect($ctrl.state.name).toEqual('opened');
-
-                $ctrl.close();
-
-                expect($ctrl.state.name).toEqual('closed');
-            });
-
-            describe('when state is opened', function () {
-                beforeEach(function () {
-                    $ctrl.toggle();
-                });
-
-                it('on start working', function () {
-                    $ctrl.startWorking();
-                    expect($ctrl.working).toBeTruthy();
-                });
-
-                it('on stop working', function () {
-                    $ctrl.startWorking();
-                    $ctrl.stopWorking();
-                    expect($ctrl.working).toBeFalsy();
-                });
-
-                describe('with actions listeners', function () {
-                    var actionId;
-
-                    beforeEach(function () {
-                        $ctrl.onShowActionsFor(function (id) {
-                            actionId = id;
-                        });
-                    });
-
-                    it('on show actions', function () {
-                        $ctrl.showActionsFor('foo');
-                        expect(actionId).toEqual('foo');
-                    });
-
-                    describe('on close', function () {
-                        beforeEach(function () {
-                            $ctrl.close();
-                        });
-
-                        it('show main actions', function () {
-                            expect(actionId).toBeUndefined();
-                        });
-                    });
-                });
-
-                it('on setButtonCode', function () {
-                    $ctrl.setButtonCode('code');
-                    expect($ctrl.buttonCode).toEqual('code');
-                });
+            it('component is in visible state', function () {
+                expect($ctrl.state.name).toEqual('visible');
             });
         });
 
@@ -127,8 +47,8 @@ describe('bin.edit module', function () {
                     $ctrl.$onChanges();
                 });
 
-                it('component is in hidden state', function () {
-                    expect($ctrl.state.name).toEqual('closed');
+                it('component is in visible state', function () {
+                    expect($ctrl.state.name).toEqual('visible');
                 });
             });
         });
@@ -139,28 +59,36 @@ describe('bin.edit module', function () {
                 topics.subscribe.calls.mostRecent().args[1](true);
             });
 
-            it('component is in hidden state', function () {
-                expect($ctrl.state.name).toEqual('closed');
+            it('component is in visible state', function () {
+                expect($ctrl.state.name).toEqual('visible');
             });
         });
 
-        it('when no main actions are subscribed', function () {
-            expect($ctrl.hasMainActions()).toBeFalsy();
-        });
+        describe('on bin.actionsevents', function() {
+            beforeEach(inject(function($rootScope) {
+                ensureEventsAreNotPropagated($rootScope);
+            }));
 
-        describe('when main actions are subscribed', function () {
-            beforeEach(function () {
-                $ctrl.increaseMainActionCount();
+            function ensureEventsAreNotPropagated($rootScope) {
+                ['bin.actions.opened', 'bin.actions.closed'].forEach(function(event) {
+                    $rootScope.$on(event, function() {
+                        fail();
+                    })
+                });
+            }
+
+            it('adds an open class to the component element when the opened event is received', function() {
+                this.$child.$emit('bin.actions.opened');
+
+                expect(this.$element.hasClass('open')).toBe(true);
             });
 
-            it('has main actions to show', function () {
-                expect($ctrl.hasMainActions()).toBeTruthy();
-            });
+            it('removes the open class again when the closed event is received', function() {
+                this.$child.$emit('bin.actions.opened');
+                this.$child.$emit('bin.actions.closed');
 
-            it('when main action is unregistered', function () {
-                $ctrl.decreaseMainActionCount();
-                expect($ctrl.hasMainActions()).toBeFalsy();
-            });
+                expect(this.$element.hasClass('open')).toBe(false);
+            })
         });
 
         describe('on destroy', function () {
@@ -172,284 +100,20 @@ describe('bin.edit module', function () {
                 var listener = topics.subscribe.calls.mostRecent().args[1];
                 expect(topics.unsubscribe).toHaveBeenCalledWith('edit.mode', listener);
             });
-        });
-    });
 
-    describe('binEditActions component', function () {
-        var $ctrl, editCtrl;
+            it('removes the listener for bin.actions.opened', function() {
+                this.$child.$emit('bin.actions.opened');
 
-        beforeEach(function () {
-            editCtrl = {
-                onShowActionsFor: jasmine.createSpy(),
-                setButtonCode: jasmine.createSpy(),
-                increaseMainActionCount: jasmine.createSpy(),
-                decreaseMainActionCount: jasmine.createSpy()
-            };
-        });
-
-        describe('when for is not defined', function () {
-            beforeEach(inject(function ($componentController) {
-                $ctrl = $componentController('binEditActions', null, {});
-                $ctrl.editCtrl = editCtrl;
-                $ctrl.$onInit();
-            }));
-
-            it('actions are visible', function () {
-                expect($ctrl.visible).toBeTruthy();
+                expect(this.$element.hasClass('open')).toBe(false);
             });
 
-            describe('on show other actions', function () {
-                beforeEach(function () {
-                    editCtrl.onShowActionsFor.calls.mostRecent().args[0]('other');
-                });
+            it('removes the listener for bin.actions.closed', function() {
+                this.$element.addClass('open');
 
-                it('actions are hidden', function () {
-                    expect($ctrl.visible).toBeFalsy();
-                });
-            });
+                this.$child.$emit('bin.actions.closed');
 
-            describe('on show no specific actions', function () {
-                beforeEach(function () {
-                    editCtrl.onShowActionsFor.calls.mostRecent().args[0]();
-                });
-
-                it('actions are visible', function () {
-                    expect($ctrl.visible).toBeTruthy();
-                });
-
-                it('set button text', function () {
-                    expect(editCtrl.setButtonCode).toHaveBeenCalledWith(undefined);
-                });
-            });
-
-            it('on increase action count', function () {
-                $ctrl.increaseActionCount();
-                expect(editCtrl.increaseMainActionCount).toHaveBeenCalled();
-            });
-
-            it('on decrease action count', function () {
-                $ctrl.decreaseActionCount();
-                expect(editCtrl.decreaseMainActionCount).toHaveBeenCalled();
-            });
-        });
-
-        describe('when for is defined', function () {
-            beforeEach(inject(function ($componentController) {
-                $ctrl = $componentController('binEditActions', null, {
-                    for: 'for'
-                });
-                $ctrl.editCtrl = editCtrl;
-                $ctrl.$onInit();
-            }));
-
-            it('actions are hidden', function () {
-                expect($ctrl.visible).toBeFalsy();
-            });
-
-            describe('on show other actions', function () {
-                beforeEach(function () {
-                    editCtrl.onShowActionsFor.calls.mostRecent().args[0]('other');
-                });
-
-                it('actions are still hidden', function () {
-                    expect($ctrl.visible).toBeFalsy();
-                });
-            });
-
-            describe('on show actions', function () {
-                beforeEach(function () {
-                    editCtrl.onShowActionsFor.calls.mostRecent().args[0]('for');
-                });
-
-                it('actions are visible', function () {
-                    expect($ctrl.visible).toBeTruthy();
-                });
-
-                it('set button text', function () {
-                    expect(editCtrl.setButtonCode).toHaveBeenCalledWith(undefined);
-                });
-            });
-
-            it('on increase action count', function () {
-                $ctrl.increaseActionCount();
-                expect(editCtrl.increaseMainActionCount).not.toHaveBeenCalled();
-            });
-
-            it('on decrease action count', function () {
-                $ctrl.decreaseActionCount();
-                expect(editCtrl.decreaseMainActionCount).not.toHaveBeenCalled();
-            });
-        });
-
-        describe('when buttonI18nCode is defined', function () {
-            beforeEach(inject(function ($componentController) {
-                $ctrl = $componentController('binEditActions', null, {
-                    for: 'for',
-                    buttonI18nCode: 'code'
-                });
-                $ctrl.editCtrl = editCtrl;
-                $ctrl.$onInit();
-            }));
-
-            describe('on show actions', function () {
-                beforeEach(function () {
-                    editCtrl.onShowActionsFor.calls.mostRecent().args[0]('for');
-                });
-
-                it('set button text', function () {
-                    expect(editCtrl.setButtonCode).toHaveBeenCalledWith('code');
-                });
-            });
-        });
-    });
-
-    describe('binEditActionsSelector component', function () {
-        var $ctrl, editCtrl, actionsCtrl;
-
-        beforeEach(inject(function ($componentController) {
-            $ctrl = $componentController('binEditActionsSelector', null, {
-                for: 'foo'
-            });
-            editCtrl = {
-                showActionsFor: jasmine.createSpy()
-            };
-            actionsCtrl = {
-                increaseActionCount: jasmine.createSpy(),
-                decreaseActionCount: jasmine.createSpy()
-            };
-            $ctrl.editCtrl = editCtrl;
-            $ctrl.actionsCtrl = actionsCtrl;
-            $ctrl.$onInit();
-        }));
-
-        it('on execute', function () {
-            $ctrl.execute();
-            expect(editCtrl.showActionsFor).toHaveBeenCalledWith('foo');
-        });
-
-        it('action is registered to actionsCtrl', function () {
-            expect(actionsCtrl.increaseActionCount).toHaveBeenCalled();
-        });
-
-        describe('on destroy', function () {
-            beforeEach(function () {
-                $ctrl.$destroy();
-            });
-
-            it('action is unregistered', function () {
-                expect(actionsCtrl.decreaseActionCount).toHaveBeenCalled();
-            });
-        });
-    });
-
-    describe('binEditAction component', function () {
-        var $ctrl, actionSpy, editCtrl, actionsCtrl;
-
-        beforeEach(inject(function ($componentController) {
-            actionSpy = jasmine.createSpy('action');
-            var bindings = {
-                action: actionSpy
-            };
-            $ctrl = $componentController('binEditAction', null, bindings);
-            editCtrl = {
-                startWorking: jasmine.createSpy(),
-                stopWorking: jasmine.createSpy()
-            };
-            actionsCtrl = {
-                increaseActionCount: jasmine.createSpy(),
-                decreaseActionCount: jasmine.createSpy()
-            };
-            $ctrl.editCtrl = editCtrl;
-            $ctrl.actionsCtrl = actionsCtrl;
-            $ctrl.$onInit();
-        }));
-
-        it('action is registered to actionsCtrl', function () {
-            expect(actionsCtrl.increaseActionCount).toHaveBeenCalled();
-        });
-
-        describe('on execute action', function () {
-            beforeEach(function () {
-                $ctrl.execute();
-            });
-
-            it('is executed', function () {
-                expect(actionSpy).toHaveBeenCalled();
-            });
-
-            describe('and action returns an object with a finally callback', function () {
-                var deferred;
-
-                beforeEach(function () {
-                    deferred = {
-                        finally: jasmine.createSpy()
-                    };
-                    actionSpy.and.returnValue(deferred);
-                    $ctrl.execute();
-                });
-
-                it('is executed', function () {
-                    expect(actionSpy).toHaveBeenCalled();
-                });
-
-                it('is working', function () {
-                    expect($ctrl.working).toBeTruthy();
-                    expect($ctrl.editCtrl.startWorking).toHaveBeenCalled();
-                });
-
-                describe('on finally', function () {
-                    beforeEach(function () {
-                        deferred.finally.calls.mostRecent().args[0]();
-                    });
-
-                    it('is not working', function () {
-                        expect($ctrl.working).toBeFalsy();
-                        expect($ctrl.editCtrl.stopWorking).toHaveBeenCalled();
-                    });
-                });
-            });
-        });
-
-        describe('when working', function () {
-            beforeEach(function () {
-                $ctrl.working = true;
-            });
-
-            describe('on execute action', function () {
-                beforeEach(function () {
-                    $ctrl.execute();
-                });
-
-                it('is not executed', function () {
-                    expect(actionSpy).not.toHaveBeenCalled();
-                });
-            });
-        });
-
-        describe('when disabled', function () {
-            beforeEach(function () {
-                $ctrl.disabled = true;
-            });
-
-            describe('on execute action', function () {
-                beforeEach(function () {
-                    $ctrl.execute();
-                });
-
-                it('is not executed', function () {
-                    expect(actionSpy).not.toHaveBeenCalled();
-                });
-            });
-        });
-
-        describe('on destroy', function () {
-            beforeEach(function () {
-                $ctrl.$destroy();
-            });
-
-            it('action is unregistered', function () {
-                expect(actionsCtrl.decreaseActionCount).toHaveBeenCalled();
-            });
+                expect(this.$element.hasClass('open')).toBe(true);
+            })
         });
     });
 });
